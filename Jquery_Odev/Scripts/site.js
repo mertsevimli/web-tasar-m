2fb6157e59c74ad504df9127d10743dfa990dc27
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2024-08-04 19:12:38 +0300

    web_design

---
+++ Jquery_Odev/Scripts/site.js
@@ -0,0 +1,14 @@
+$("#getir").click(function () {
+    $.ajax({
+      url: "/Ajaxicerik.txt", // ajaz isteğini göndereceğimiz site
+      success: function (sonuc) {
+        //işlem  başaralı olursa çalısacak kod bloğu
+        $("#ajax").html(sonuc);
+      },
+      error: function () {
+        // işlem başarısız olursa çalışacak kod bloğu
+        $("#ajax").html("hata oluştu");
+        console.error("hata oluştu");
+      },
+    });
+  });
