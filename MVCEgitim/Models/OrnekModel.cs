038d908447d395551d0b3ee5ecc69798ecec4c74
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2024-08-11 12:27:52 +0300

    Layout-2

---
+++ MVCEgitim/Models/OrnekModel.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCEgitim.Models
+{
+    public class OrnekModel
+    {
+        
+    }
+}
