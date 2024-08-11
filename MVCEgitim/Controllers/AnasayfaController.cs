22d156b083d12a635c1fc18268525d49beb57e24
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2024-08-11 10:22:25 +0300

    11 Ağustos

---
+++ MVCEgitim/Controllers/AnasayfaController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MVCEgitim.Controllers
+{
+    
+    public class AnasayfaController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+            public IActionResult Hakkimizda()// 1 controller da 1 den fazla action tanımlayabiliiyoruz.
+        {
+            return View();// Burada View altında Hakkımızzda adında bir view
+        }
+
+        
+    }
+}
--- NisanEgitimiMVCEgitimi/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace NisanEgitimiMVCEgitimi.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
