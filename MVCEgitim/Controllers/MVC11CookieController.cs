87ff2b1f8767752e32055ada680535a97fa9a85f
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2024-09-07 10:43:13 +0300

    Cookie

--- MVCEgitim/Controllers/MVC11CookieController.cs
+++ MVCEgitim/Controllers/MVC11CookieController.cs
@@ -29,8 +29,21 @@ namespace MVCEgitim.Controllers
             var kullanici = _context.Uyeler.FirstOrDefault(u =>
                 u.KullaniciAdi == kullaniciAdi && u.Sifre == sifre
             );
-            if (kullanici != null) { }
-
+            if (kullanici != null)
+            {
+                // tarayıcıda 1 cookie oluştur.
+                CookieOptions cookieOptions =
+                    new() // oluşacak cookie ayarlarını yapılandırmak istersek bunu kullanıyoruz.
+                    {
+                        Expires = DateTime.Now.AddMinutes(1),
+                    };
+                Response.Cookies.Append("kullaniciAdi", kullaniciAdi, cookieOptions); //* çerez kaydet
+                Response.Cookies.Append("sifre", sifre, cookieOptions); //* çerezi kaydet
+                HttpContext.Response.Cookies.Append("userguid", Guid.NewGuid().ToString()); //* userguid isminde 1 çerez daha oluştur ve içinde o kullanıcıya özel şifreli bir değer sakla
+                return RedirectToAction("CookieOku");
+            }
+else
+TempData["Mesaj"]=@"<div class ='alert alert danger'> Giriş Başarısız! </div>";
             return View();
         }
     }
