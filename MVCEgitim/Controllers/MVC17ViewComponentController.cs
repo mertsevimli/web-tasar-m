0257d688a9499a08322e22921cdeae35403f3b77
mertsevimli <136606862+mertsevimli@users.noreply.github.com>
2024-09-14 10:27:35 +0300

    14.09.2024

--- MVCEgitim/Controllers/MVC17ViewComponentController.cs
+++ MVCEgitim/Controllers/MVC17ViewComponentController.cs
@@ -5,13 +5,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MVCEgitim.Models;
 
 namespace MVCEgitim.Controllers
 {
     
     public class MVC17ViewComponentController : Controller
     {
-       
+       private readonly UyeContext _context;
+
+       public MVC17ViewComponentController(UyeContext context)
+       {
+        _context = context;
+       }
 
         public IActionResult Index()
         {
